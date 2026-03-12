ec61b48d720575b0f6f6fde768af0ae6017ef990
MouhyiDS <mouhyidinnes@gmail.com>
2026-03-12 22:39:06 +0000

    setting up the socket-user mapping for connection and disconnection

--- Back-end/socket/socket.js
+++ Back-end/socket/socket.js
@@ -1,16 +1,25 @@
 const messageSocket = require("./messageSocket");
 const roomSocket = require("./roomSocket");
 
+const onlineUsers = new Map();
 module.exports = (io) => {
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+
+    socket.on("register_user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
 
     roomSocket(io, socket);
     messageSocket(io, socket);
 
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
+
+      socket.on("register_user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
     });
 
   });
